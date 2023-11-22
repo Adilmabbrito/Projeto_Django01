05b7b67e44a5ea24a3370c8e33e8f23f65ef0aec
Adilma Barbosa <adilma188@yahoo.com.br>
2023-11-22 15:37:32 -0300

    criando os models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title=models.CharField(max_length= 180)
+    image= models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
+    
+class Marca(models.Model):
+    title=models.CharField(max_length= 100)
+    image= models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
+
+class Color(models.Model):
+    title=models.CharField(max_length= 180)
+    color_codigo= models.CharField(max_length= 100)
+    def __str__(self):
+        return self.title
+    
+class Size(models.Model):
+    title=models.CharField(max_length= 180)
+    
+    def __str__(self):
+        return self.title
+    
